514a2595e791e7e7093fe724adf26ef7c9702607
[HippieTaoist] <[shawn.m.kittel@gmail.com]>
2021-11-25 16:53:14 -0700

    initial commit with AuthContextComponent wrapping it

---
+++ src/context/AuthContext.js
@@ -0,0 +1,36 @@
+import React, { useContext, useReducer } from "react";
+
+export const AuthContext = React.createContext({});
+const initialState = { user: null };
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "LOGIN":
+      return {
+        user: {
+          email: action.email,
+          username: action.username,
+          isAuth: true,
+        },
+      };
+    case "LOGOUT":
+      return {
+        user: null,
+      };
+
+    default:
+      return state;
+  }
+}
+
+function AuthContextComponent({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <AuthContext.Provider value={{ state, dispatch }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export default AuthContextComponent;
