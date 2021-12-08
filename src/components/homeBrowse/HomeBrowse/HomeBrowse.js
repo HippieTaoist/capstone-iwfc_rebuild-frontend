f099c04196df603d5bfe3ab8f386593256dd533e
hippietaoist <Shawn.M.Kittel@gmail.com>
2021-12-07 20:53:13 -0700

    add two components - DisplayCryptoPrograms & DisplayCryptoCoins

--- src/components/homeBrowse/HomeBrowse/HomeBrowse.js
+++ src/components/homeBrowse/HomeBrowse/HomeBrowse.js
@@ -1,4 +1,6 @@
 import React from "react";
+import DisplayCryptoCoin from "../DisplayCryptoCoins/DisplayCryptoCoins";
+import DisplayCryptoPrograms from "../DisplayCryptoPrograms/DisplayCryptoPrograms";
 
 import "./HomeBrowse.css";
 
@@ -6,6 +8,14 @@ export default function HomeBrowse() {
   return (
     <div className="HomeBrowse">
       <h3>I'm a HomeBrowse Function</h3>
+      <div className="siteCryptoDisplayContainer">
+        <div className="cryptoDisplayContainer">
+          <DisplayCryptoCoin />
+        </div>
+        <div className="cryptoProgramDisplayContainer">
+          <DisplayCryptoPrograms />
+        </div>
+      </div>
     </div>
   );
 }
