5bb845891da0198ed45b612fc279e163af393965
hippietaoist <Shawn.M.Kittel@gmail.com>
2021-12-08 19:27:32 -0700

    add css

--- src/components/crypto/CryptoPrices/CryptoPrices.js
+++ src/components/crypto/CryptoPrices/CryptoPrices.js
@@ -1,8 +1,10 @@
-import React, { useContext } from "react";
+import React, { useContext, useEffect } from "react";
 // import axios from "axios";
 // import { kuCoinAPI } from "../../../utils/CryptoAPIs/CryptoAPIs";
 import AxiosBackend from "../../../utils/axios/AxiosBackend";
 import { CryptoContext } from "../../../context/CryptoContext";
+
+import "./CryptoPrices.css";
 
 export default function CryptoPrices() {
   const {
@@ -12,9 +14,9 @@ export default function CryptoPrices() {
     },
   } = useContext(CryptoContext);
 
-  async function handleOnClick() {
+  async function loader() {
     try {
-      console.log(siteCrypto);
+      // console.log(siteCrypto);
       let crypto = await AxiosBackend.get("/api/cryptos/", {});
       // console.log(crypto);
       let dispatchedPriceArray = crypto.data.payload;
@@ -23,42 +25,38 @@ export default function CryptoPrices() {
         type: "SiteCryptoSet",
         siteCrypto: dispatchedPriceArray,
       });
-      // console.log(crypto);
-      // console.log(crypto.data);
-      // console.log(crypto.data.message);
-      // console.log(crypto.data.payload);
-      // console.log(cryptoPriceArray);
-      //   let siteCryptoArray = crypto.data.payload;
-      // console.log(crypto.data.pricePayload);
-      // let incomingCryptoPriceArray = crypto.data.pricePayload;
-      // console.log(incomingCryptoPriceArray);
-      //   dispatch({
-      //     type: "PriceSet",
-      //     cryptoPriceArray: crypto.data.pricePayload,
-      //   });
       console.log(siteCrypto);
     } catch (error) {
       console.log(error);
     }
   }
 
-  // useEffect(() => {}, []);
+  useEffect(() => {
+    loader();
+  }, []);
 
   return (
     <div>
       I Am a CryptoPrices Div
-      <button onClick={handleOnClick}>click to see our coins</button>
+      {/* <button onClick={handleOnClick}>click to see our coins</button> */}
       <hr />
       {siteCrypto.length > 0 ? (
-        <div>
+        <div className="CryptoPrices-container-main">
           {siteCrypto.map((item) => {
-            for (const attr in item) {
-              console.log(attr);
-              if (attr === "priceCurrent") {
-                return <div>${item[attr]}</div>;
-              }
-              return "";
-            }
+            return (
+              <div key={item._id} className="CryptoPrices-container-crypto">
+                <div>
+                  <img src={item.logo} alt={item.name} />
+                </div>
+                <div className="CryptoPrices-name">{item.name}</div>
+                <div className="CryptoPrices-price">
+                  ${" "}
+                  {item.priceCurrent > 1
+                    ? (Math.round(item.priceCurrent * 100) / 100).toFixed(2)
+                    : item.priceCurrent}
+                </div>
+              </div>
+            );
           })}
         </div>
       ) : (
