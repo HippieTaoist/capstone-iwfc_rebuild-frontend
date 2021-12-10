7f0d50300b920a1c4bce50fbf7bf6392025d3456
hippietaoist <Shawn.M.Kittel@gmail.com>
2021-12-10 13:42:33 -0700

    organize info a bit with cvss and classnames

--- src/components/crypto/CryptoDetails/CryptoDetails.js
+++ src/components/crypto/CryptoDetails/CryptoDetails.js
@@ -5,6 +5,8 @@ import SiteTools from "../../../utils/siteTools/loader/SiteTools";
 import AxiosBackend from "../../../utils/axios/AxiosBackend";
 import { CryptoContext } from "../../../context/CryptoContext";
 
+import "./CryptoDetails.css";
+
 export default function CryptoDetails() {
   const {
     dispatch,
@@ -16,21 +18,6 @@ export default function CryptoDetails() {
   const crypto = useParams();
   const _id = crypto.id;
   console.log(_id);
-
-  async function loader() {
-    try {
-      let crypto = await AxiosBackend.get("/api/cryptos/");
-      let dispatchedPriceArray = crypto.data.payload;
-      console.log(dispatchedPriceArray);
-      dispatch({
-        type: "SiteCryptoSet",
-        siteCrypto: dispatchedPriceArray,
-      });
-      console.log(siteCrypto);
-    } catch (error) {
-      console.log(error);
-    }
-  }
 
   let foundCrypto = siteCrypto.find((item) => {
     return item._id === _id ? item : null;
@@ -60,10 +47,173 @@ export default function CryptoDetails() {
 
   return (
     <div className="CryptoDetails-container-main">
-      <h1>IT"S WORKING</h1>
+      <h1>This is your {foundCrypto.category}: </h1>
+
       <div className="CryptoDetails-crypto-details-container">
-        <div className="CryptoDetails-logo">{/* <img src={} /> */}</div>
+        <div className="CryptoDetails-left-container">
+          <div className="CryptoDetails-logo">
+            {<img src={foundCrypto.logo} />}
+          </div>
+          <div className="CryptoDetails-symbol">
+            <h3>{foundCrypto.symbol}</h3>
+          </div>
+          <div className="CryptoDetails-priceCurrent">
+            <h3>
+              {foundCrypto.priceCurrent > 1
+                ? (Math.round(foundCrypto.priceCurrent * 100) / 100).toFixed(2)
+                : foundCrypto.priceCurrent}
+            </h3>
+          </div>
+        </div>
+        <div className="CryptoDetails-right-container">
+          <div className="CryptoDetails-name">
+            <h3>{foundCrypto.name}</h3>
+          </div>
+          <div className="CryptoDetails-description">
+            <p>{foundCrypto.description}</p>
+          </div>
+          <div className="CryptoDetails-container-favored">
+            <div className="CryptoDetails-usersFavored">
+              hodl: {foundCrypto.usersFavored.length}
+            </div>
+            <div className="CryptoDetails-usersUnfavored">
+              fodl: {foundCrypto.usersUnfavored.length}
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <hr />
+
+      <h2>More Research / Reach-Out Locations:</h2>
+      <div className="CryptoDetails-container-research-reachout">
+        <div>
+          {" "}
+          Website(s):{" "}
+          {foundCrypto.urls.website.map((item) => {
+            return (
+              <div className="CryptoDetails-website">
+                <a href={item} className="CryptoDetails-container-links">
+                  {item}
+                </a>
+                <br />
+              </div>
+            );
+          })}
+        </div>
+        <div>
+          {" "}
+          Explorer(s):{" "}
+          {foundCrypto.urls.explorer.map((item) => {
+            return (
+              <div className="CryptoDetails-explorer">
+                <a href={item} className="CryptoDetails-container-links">
+                  {item}
+                </a>
+                <br />
+              </div>
+            );
+          })}
+        </div>
+        <div>
+          {" "}
+          Docs:
+          <br />
+          <div>
+            Source Code:{" "}
+            {foundCrypto.urls.source_code.map((item) => {
+              return (
+                <div className="CryptoDetails-source_code">
+                  <a href={item} className="CryptoDetails-container-links">
+                    {item}
+                  </a>
+                </div>
+              );
+            })}
+          </div>{" "}
+          <div>
+            Technical Documents:{" "}
+            {foundCrypto.urls.technical_doc.map((item) => {
+              return (
+                <div className="CryptoDetails-technical_doc">
+                  <a href={item} className="CryptoDetails-container-links">
+                    {item}
+                  </a>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div>
+          {" "}
+          Social Media:
+          <br />{" "}
+          <div>
+            Facebook:{" "}
+            {foundCrypto.urls.facebook.map((item) => {
+              return (
+                <div className="CryptoDetails-facebook">
+                  <a href={item} className="CryptoDetails-container-links">
+                    {item}
+                  </a>
+                </div>
+              );
+            })}
+          </div>
+          <div>
+            Twitter: {foundCrypto.urls.twitter_username}
+            <br />
+            {foundCrypto.urls.twitter.map((item) => {
+              return (
+                <div className="CryptoDetails-twitter_username">
+                  <a href={item} className="CryptoDetails-container-links">
+                    {item}
+                  </a>
+                </div>
+              );
+            })}
+          </div>
+          <div>
+            reddit:{" "}
+            {foundCrypto.urls.reddit.map((item) => {
+              return (
+                <div className="CryptoDetails-reddit">
+                  <a href={item} className="CryptoDetails-container-links">
+                    {item}
+                  </a>
+                </div>
+              );
+            })}
+          </div>
+        </div>
+        <div>
+          {" "}
+          Message Boards:{" "}
+          {foundCrypto.urls.message_board.map((item) => {
+            return (
+              <div className="CryptoDetails-message_board">
+                <a href={item} className="CryptoDetails-container-links">
+                  {item}
+                </a>
+              </div>
+            );
+          })}
+        </div>
+        <div>
+          {" "}
+          Chat:{" "}
+          {foundCrypto.urls.chat.map((item) => {
+            return (
+              <div className="CryptoDetails-chat">
+                <a href={item} className="CryptoDetails-container-links">
+                  {item}
+                </a>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
+    // </div>
   );
 }
