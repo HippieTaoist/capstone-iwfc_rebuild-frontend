f1097db4cb4b6f983b7779707074136c4631fbbf
hippietaoist <Shawn.M.Kittel@gmail.com>
2021-12-16 09:16:05 -0700

    add cryptoprogramcontext with initial code and setup in index.js'

---
+++ src/context/CryptoProgramContext.js
@@ -0,0 +1,30 @@
+import React, { useReducer } from "react";
+
+export const CryptoProgramContext = React.createContext({});
+const initialState = {
+  CryptoProgram: { siteCryptoProgram: [] },
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case "SiteCryptoProgramSet":
+      return {
+        CryptoProgram: { siteCryptoProgram: action.siteCryptoProgram },
+      };
+
+    default:
+      return state;
+  }
+}
+
+function CryptoProgramContextComponent({ children }) {
+  const [state, dispatch] = useReducer(reducer, initialState);
+
+  return (
+    <CryptoProgramContext.Provider value={{ state, dispatch }}>
+      {children}
+    </CryptoProgramContext.Provider>
+  );
+}
+
+export default CryptoProgramContextComponent;
--- src/index.js
+++ src/index.js
@@ -4,11 +4,20 @@ import "./index.css";
 import App from "./App";
 
 import AuthContextComponent from "./context/AuthContext";
+import CryptoContextComponent from "./context/CryptoContext";
+import UserContextComponent from "./context/UserContext";
+import CryptoProgramContextComponent from "./context/CryptoProgramContext";
 
 ReactDOM.render(
   <React.StrictMode>
     <AuthContextComponent>
-      <App />
+      <CryptoProgramContextComponent>
+        <CryptoContextComponent>
+          <UserContextComponent>
+            <App />
+          </UserContextComponent>
+        </CryptoContextComponent>
+      </CryptoProgramContextComponent>
     </AuthContextComponent>
   </React.StrictMode>,
   document.getElementById("root")
